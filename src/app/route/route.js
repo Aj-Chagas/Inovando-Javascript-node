46413d1ae0e86b3568397e7537438bd664d22be9
andersonchagas <anderson.1997reis@gmail.com>
2020-05-26 16:08:46 -0300

    templates dinâmicos

--- src/app/route/route.js
+++ src/app/route/route.js
@@ -2,8 +2,21 @@ module.exports = (app) => {
 
     app.get('/', (req, resp) => {
         resp.marko(
-            require('../view/book/list/list.marko')
+            require('../view/book/list/list.marko'),
+            {
+                livros: [
+                    {
+                        id:1,
+                        title: 'Fundamentos do node'
+                    },
+                    {
+                        id: 2,
+                        title: 'Node avançado'
+                    }
+                ]
+            }
         )
     })
 }
 
+ 
